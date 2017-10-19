a1723908f4f1a067358d1a6128471e6956596e59
Elijah Manor <elijah.manor@gmail.com>
2017-10-18 22:05:29 -0500

    getUserInfo HOC

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -4,57 +4,61 @@ import Profile from "../../components/Profile/Profile";
 import Fuses from "../../components/Fuses/Fuses";
 import Ads from "../../components/Ads/Ads";
 import Footer from "../../components/Footer/Footer";
+import Compose from "../../components/Compose/Compose";
 import { getUser } from "../../services/user";
 import { getFuses, updateFuse, addFuse } from "../../services/fuse";
-
-import Compose from "../../components/Compose/Compose";
 
 import "../../index.css";
 import "./App.css";
 
+const getUserInfo = WrappedComponent => {
+  return class GetUserInfo extends Component {
+    state = { user: {} };
+    componentDidMount() {
+      getUser().then(user => this.setState({ user }));
+    }
+    render() {
+      return <WrappedComponent {...this.props} {...this.state} />;
+    }
+  };
+};
+
+const WrappedProfile = getUserInfo(Profile);
+
 export default class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      user: {},
-      fuses: [],
-      showModal: false
-    };
-    this.handleNew = this.handleNew.bind(this);
-    this.handleClose = this.handleClose.bind(this);
-    this.handleCompose = this.handleCompose.bind(this);
-    this.handleBomb = this.handleBomb.bind(this);
-    this.fetchFuses = this.fetchFuses.bind(this);
-  }
+  state = {
+    user: {},
+    fuses: [],
+    showModal: false
+  };
   componentDidMount() {
-    getUser().then(user => this.setState({ user }));
     this.fetchFuses();
   }
-  fetchFuses() {
+  fetchFuses = () => {
     getFuses().then(fuses => this.setState({ fuses }));
-  }
-  handleBomb({ id, bomb }, e) {
+  };
+  handleBomb = ({ id, bomb }, e) => {
     updateFuse({ id, bomb }).then(this.fetchFuses);
-  }
-  handleNew() {
+  };
+  handleNew = () => {
     this.setState({ showModal: true });
-  }
-  handleCompose(message) {
+  };
+  handleCompose = message => {
     addFuse({
       message,
       user: this.state.user
     }).then(this.fetchFuses);
     this.setState({ showModal: false });
-  }
-  handleClose() {
+  };
+  handleClose = () => {
     this.setState({ showModal: false });
-  }
+  };
   render() {
     const { user, fuses, showModal } = this.state;
     return (
       <div className="App">
         <Toolbar className="App-toolbar" onNew={this.handleNew} />
-        <Profile className="App-profile" user={user} />
+        <WrappedProfile className="App-profile" />
         <Fuses className="App-list" fuses={fuses} onBomb={this.handleBomb} />
         <Ads className="App-ads" />
         <Footer className="App-footer" />
